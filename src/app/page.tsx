591d34d3b566b3e0e591c554421c70a2e1540868
mdasadulgoni <mdasadulgoni@gmail.com>
2023-11-24 12:52:43 +0530

    commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// 1. Import Area
 
-export default function Home() {
+
+// 2. Defination Area
+function Home() {
+  // 2.1 Hooks Area
+
+  // 2.2 Defination Area
+  // We can use server actions inside the serve component function defination
+  async function asad(formData: FormData) {
+    'use server'
+    console.log(formData);
+    const fn = formData.get('firstname');
+    console.log(fn);  
+  }
+
+  // 2.3 Return Area
   return (
     <>
-      <h1>Server Component</h1>
+        <div className="container">
+        <h1 className="text-center mb-5 mt-5">Student Admition Form</h1>
+          <form method="GET" action={asad}>    
+            <div className="mb-6">
+                <input type="text" name="firstname" className="form-control mb-4" id="exampleInputEmail1" />
+                <div className="text-center">
+                   <button type="submit" className="btn btn-primary text-center">Submit</button>
+                </div>
+            </div>
+          </form>
+        </div>
+      
+
     </>
   )
 }
+// 3. Export Area
+  // 3.1 Default Export
+  export default Home;
+  // 3.2 Named Export
