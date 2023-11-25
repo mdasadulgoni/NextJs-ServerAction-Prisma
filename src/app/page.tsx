28f98f4361cb7a094a6a0f7ae70f5f505a86d281
mdasadulgoni <mdasadulgoni@gmail.com>
2023-11-25 06:15:18 +0530

    c2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 // 1. Import Area
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 // 2. Defination Area
-function Home() {
+async function Home() {
   // 2.1 Hooks Area
 
   // 2.2 Defination Area
@@ -12,14 +13,22 @@ function Home() {
     console.log(formData);
     const fn = formData.get('firstname');
     console.log(fn);  
+    try {
+      const student = await prisma.students_tbl.create({
+        data: {
+          firstname :""+firstname,
+        }
+      })
+    } catch (error) {
+      
+    }
   }
-
   // 2.3 Return Area
   return (
     <>
         <div className="container">
         <h1 className="text-center mb-5 mt-5">Student Admition Form</h1>
-          <form method="GET" action={asad}>    
+          <form method="POST" action={asad}>    
             <div className="mb-6">
                 <input type="text" name="firstname" className="form-control mb-4" id="exampleInputEmail1" />
                 <div className="text-center">
